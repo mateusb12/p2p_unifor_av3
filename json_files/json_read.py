5ccf096dd0170a5385b3da0544f77e5747cf1004
Mateus <mateus_b09@hotmail.com>
2023-11-23 12:17:23 -0300

    Add new JSON file, JSON reader and file path references
    This commit introduces three new files to handle a specified JSON structure. The `json_example.json` contains an example of the expected JSON data, with nodes representing tracks and their relations. `json_read.py` provides functionality to read and parse this JSON data, checking validity of node and neighbor counts and ensuring correct datatypes. `all_references.py` contains functions to handle file path references. This set of changes supports the introduction of JSON data management in the application.

---
+++ json_files/json_read.py
@@ -0,0 +1,90 @@
+from typing import Tuple
+
+from path_references.all_references import get_json_files_path
+import json
+from pathlib import Path
+
+
+def _read_json(filename: str) -> dict or None:
+    path = get_json_files_path() / filename
+    try:
+        with open(path, 'r') as file:
+            json_data = json.load(file)
+        return json_data
+    except FileNotFoundError:
+        print(f"Arquivo {filename} não encontrado.")
+        return None
+    except json.JSONDecodeError:
+        print("Erro ao decodificar JSON.")
+        return None
+
+
+def _validate_node_neighborhood(neighbors: list, min_neighbors: int, max_neighbors: int) -> bool:
+    return min_neighbors <= len(neighbors) <= max_neighbors
+
+
+def _parse_json(json_data: dict) -> Tuple[bool, str]:
+    if json_data is None:
+        return False, "File not found."
+
+    num_nodes = json_data.get("num_nodes")
+    min_neighbors = json_data.get("min_neighbors")
+    max_neighbors = json_data.get("max_neighbors")
+    resources = json_data.get("resources", {})
+    edges = json_data.get("edges", [])
+
+    # Verificações básicas
+    if not isinstance(num_nodes, int):
+        return False, "Invalid data type for num_nodes. It should be integer."
+
+    if not isinstance(min_neighbors, int):
+        return False, "Invalid data type for min_neighbors. It should be integer."
+
+    if not isinstance(max_neighbors, int):
+        return False, "Invalid data type for max_neighbors. It should be integer."
+
+    if not isinstance(resources, dict):
+        return False, "Invalid data type for resources. It should be dict."
+
+    if not isinstance(edges, list):
+        return False, "Invalid data type for edges. It should be list."
+
+    # Verifica se o número de nós e vizinhos está dentro dos limites
+    if not (0 < num_nodes <= 1000):
+        return False, "Invalid number of nodes. It should be between 1 and 1000."
+
+    if not (0 <= min_neighbors <= max_neighbors):
+        return False, f"Invalid number of neighbors. min_neighbors should be less than or equal to {max_neighbors}."
+
+    # Constrói um dicionário para contar os vizinhos de cada nó
+    neighbor_count = {node: [] for node in resources}
+    for edge in edges:
+        if edge.get("from") in neighbor_count and edge.get("to") in neighbor_count:
+            neighbor_count[edge["from"]].append(edge["to"])
+        else:
+            return False, f"Invalid node in edge: {edge}"
+
+    # Verifica se todos os nós têm o número correto de vizinhos
+    for node, neighbors in neighbor_count.items():
+        if not _validate_node_neighborhood(neighbors, min_neighbors, max_neighbors):
+            return False, (f"Invalid number of neighbors for node {node}."
+                           f" It should be between {min_neighbors} and {max_neighbors}.")
+
+    return True, "Valid JSON"
+
+
+def read_and_parse_json(filename: str) -> dict or str:
+    json_content = _read_json(filename)
+    json_parse_result, json_parse_explanation = _parse_json(json_content)
+    if json_parse_result:
+        return json_content
+    return json_parse_explanation
+
+
+def __main():
+    json_data = read_and_parse_json("json_example.json")
+    return
+
+
+if __name__ == '__main__':
+    __main()
---
+++ path_references/all_references.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+
+def get_main_folder_path() -> Path:
+    """
+    Returns the path of the main folder of the project.
+    :return: Path
+    """
+    return Path(__file__).parent.parent
+
+
+def get_json_files_path() -> Path:
+    """
+    Returns the path of the folder containing the json files.
+    :return: Path
+    """
+    return get_main_folder_path() / "json_files"
+
+
+def __main():
+    path = get_json_files_path()
+    return
+
+
+if __name__ == '__main__':
+    __main()
