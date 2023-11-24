51c4d03ca6cc596e964027486714958cf4120be4
Mateus <mateus_b09@hotmail.com>
2023-11-24 19:16:44 -0300

    Update network graph visualization and tracking of visited nodes
    The network graph visualization in 'visualizations.py' has been refactored to improve user experience and debugging processes. It now visually distinguishes visited nodes using a different color for easy tracking.
    
    A new parameter 'visited_nodes' has been added to the 'plotly_networkx' function to enable tracking of which nodes have been visited in the network. This ensures better understanding of network flow and helps uncover potential issues with node traversal.
    
    Additionally, sliders and frame animations have been put in place to illustrate the transition of the visited state for nodes. This further enhances the user's ability to manually trace node visitation order in the network.

--- network_visualization/visualizations.py
+++ network_visualization/visualizations.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import networkx as nx
 from json_files.json_read import read_and_parse_json
 from network_structure.graph_object import Graph
@@ -15,7 +17,7 @@ def _convert_to_networkx(input_graph: Graph) -> nx.Graph:
     return G
 
 
-def plotly_networkx(graph: nx.Graph):
+def plotly_networkx(graph: nx.Graph, visited_nodes: List[str] = None):
     pos = nx.spring_layout(graph)
 
     edge_x = []
@@ -38,25 +40,69 @@ def plotly_networkx(graph: nx.Graph):
         resources_info = '<br>'.join(graph.nodes[node]['info'])
         node_resources.append(resources_info)
 
-    node_marker_style = dict(showscale=False, color='LightSkyBlue', size=55, line=dict(width=2, color='RoyalBlue'))
+    node_blue_marker_style = dict(showscale=False, color='LightSkyBlue', size=55, line=dict(width=2, color='RoyalBlue'))
+    node_red_marker_style = dict(showscale=False, color='LightCoral', size=55, line=dict(width=2, color='Crimson'))
     hover_label_style = dict(bgcolor='DarkOrange', font=dict(color='LightYellow', size=13))
 
-    node_trace = go.Scatter(x=node_x, y=node_y, mode='markers', hoverinfo='text', hovertemplate='%{text}',
-                            text=node_resources, marker=node_marker_style, hoverlabel=hover_label_style)
+    node_trace_original = go.Scatter(x=node_x, y=node_y, mode='markers', hoverinfo='text', hovertemplate='%{text}',
+                                     text=node_resources, marker=node_blue_marker_style, hoverlabel=hover_label_style)
+
+    node_trace_red = go.Scatter(x=node_x, y=node_y, mode='markers', hoverinfo='text', hovertemplate='%{text}',
+                                text=node_resources, marker=node_red_marker_style, hoverlabel=hover_label_style)
 
     text_trace_format = [f'•<br>{node}' for node in graph.nodes()]
     text_trace = go.Scatter(x=node_x, y=node_y, mode='text', text=text_trace_format,
                             hoverinfo='none', textposition="middle center", textfont=dict(color='Black', size=9))
 
-    fig = go.Figure(data=[edge_trace, node_trace, text_trace],
-                    layout=go.Layout(title='<br>Network graph made with Python', titlefont=dict(size=16),
-                                     showlegend=False, hovermode='closest', margin=dict(b=20, l=5, r=5, t=40),
-                                     annotations=[dict(text="Python code: <a href='https://plotly.com/'> Plotly</a>",
-                                                       showarrow=False, xref="paper", yref="paper", x=0.005, y=-0.002)],
-                                     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                                     paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)'))
+    layout = go.Layout(title='<br>Network graph made with Python', titlefont=dict(size=16),
+                       showlegend=False, hovermode='closest', margin=dict(b=20, l=5, r=5, t=40),
+                       annotations=[dict(text="Python code: <a href='https://plotly.com/'> Plotly</a>",
+                                         showarrow=False, xref="paper", yref="paper", x=0.005, y=-0.002)],
+                       xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                       yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                       paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
+    fig = go.Figure(data=[edge_trace, node_trace_red, text_trace],
+                    layout=layout)
+
+    frames = [
+        go.Frame(data=[edge_trace, node_trace_original, text_trace], name='frame1'),
+        go.Frame(data=[edge_trace, node_trace_red, text_trace], name='frame2')
+    ]
+
+    fig.frames = frames
+
+    sliders_dict = {
+        "active": 0,
+        "yanchor": "top",
+        "xanchor": "left",
+        "currentvalue": {
+            "font": {"size": 20},
+            "prefix": "Visited:",
+            "visible": True,
+            "xanchor": "right"
+        },
+        "transition": {"duration": 300, "easing": "cubic-in-out"},
+        "pad": {"b": 10, "t": 50},
+        "len": 0.9,
+        "x": 0.1,
+        "y": 0,
+        "steps": []
+    }
+
+    for i, node in enumerate(visited_nodes):
+        slider_step = {"args": [
+            [f"frame{i + 1}"],
+            {"frame": {"duration": 1000, "redraw": True},
+             "mode": "immediate",
+             "transition": {"duration": 300}}
+        ],
+            "label": node,
+            "method": "animate"}
+        sliders_dict["steps"].append(slider_step)
+
+    fig.update_layout(sliders=[sliders_dict])
+    fig.update_layout(updatemenus=[dict(type="buttons", buttons=[dict(label="Play", method="animate", args=[None])])])
     pyo.iplot(fig)
 
 
@@ -65,7 +111,8 @@ def visualize_graph():
     g = Graph(json_data)
     nx_graph = _convert_to_networkx(g)
     nx_graph_edges = list(nx_graph.edges)
-    plotly_networkx(nx_graph)
+    visited_nodes = ['node_1', 'node_2', 'node_3']
+    plotly_networkx(nx_graph, visited_nodes)
     # net = pyvisNetwork(notebook=False, width="100%", height="700px", bgcolor="#222222", font_color="white")
     # _create_pyvis_graph(net, nx_graph, g, 'output_graph.html')
 
