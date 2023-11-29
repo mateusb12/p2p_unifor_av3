9f01635c119802cb5c88cff9e68c9705dbd1831e
Ísis Ximenes <isisximenes@hotmail.com>
2023-11-29 14:55:38 -0300

    v1 Update random_walk_search.py

--- network_search/random_walk_search.py
+++ network_search/random_walk_search.py
@@ -3,11 +3,30 @@ from network_parse.dfs_parse import parse_graph
 from network_structure.graph_object import Graph
 
 
-def start_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResource: str, initial_ttl: int = 5) -> dict:
-    visited_nodes = []
-    ttl_history = []
+def start_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResource: str,
+                             initial_ttl: int = 5) -> dict:
+    visited_nodes = [start_node_id]
+    ttl_history = [initial_ttl]
 
-    # TODO: Implement random walk search
+    current_node = start_node_id
+    ttl = initial_ttl
+
+    while ttl >= 0:
+        neighbors = inputGraph.get_neighbors(current_node)
+
+        if not neighbors:
+            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
+
+        next_node = random.choice(neighbors)
+
+        current_node = next_node
+        ttl -= 1
+
+        visited_nodes.append(current_node)
+        ttl_history.append(ttl)
+
+        if desiredResource in inputGraph.get_node_resources(current_node):
+            return {"visited": visited_nodes, "found": True, "ttl_history": ttl_history}
 
     return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
 
