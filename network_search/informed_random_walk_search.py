d2f1644b4c0888bdec8a2314b323691b5000cf47
Ísis Ximenes <isisximenes@hotmail.com>
2023-11-29 14:56:41 -0300

    Update informed_random_walk_search.py

--- network_search/informed_random_walk_search.py
+++ network_search/informed_random_walk_search.py
@@ -5,10 +5,31 @@ from network_structure.graph_object import Graph
 
 def start_informed_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResource: str,
                                       initial_ttl: int = 5) -> dict:
-    visited_nodes = []
-    ttl_history = []
+    visited_nodes = [start_node_id]
+    ttl_history = [initial_ttl]
 
-    # TODO: Implement informed random walk search
+    current_node = start_node_id
+    ttl = initial_ttl
+
+    while ttl > 0:
+        neighbors = inputGraph.get_neighbors(current_node)
+
+        neighbors_with_resource = [neighbor for neighbor in neighbors
+                                   if desiredResource in inputGraph.get_node_resources(neighbor)]
+
+        if not neighbors_with_resource:
+            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
+
+        next_node = random.choice(neighbors_with_resource)
+
+        current_node = next_node
+        ttl -= 1
+
+        visited_nodes.append(current_node)
+        ttl_history.append(ttl)
+
+        if desiredResource in inputGraph.get_node_resources(current_node):
+            return {"visited": visited_nodes, "found": True, "ttl_history": ttl_history}
 
     return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
 
