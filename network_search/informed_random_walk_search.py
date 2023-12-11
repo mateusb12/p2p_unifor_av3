e1e0cd3a0936f0af806658407eb991400601d98d
isisximenes <isisximenes@hotmail.com>
2023-12-11 13:06:44 -0300

    Update informed_random_walk_search.py

--- network_search/informed_random_walk_search.py
+++ network_search/informed_random_walk_search.py
@@ -1,70 +1,45 @@
 from json_parsing.json_read import read_json
 from network_parse.dfs_parse import parse_graph
 from network_structure.graph_object import Graph
+from typing import List
 import random
 
-def get_neighbors(self, node_id):
-        #
-        return self.graph_data[node_id]['neighbors']
-
-def get_node_resources(self, node_id):
-        #
-        return self.graph_data[node_id].get('resources', [])
-
-def update_cache(self, node_id, result):
-        self.cache[node_id] = result
-
-def get_cached_result(self, node_id):
-        return self.cache.get(node_id)
 
 def start_informed_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResource: str,
                                       initial_ttl: int = 5) -> dict:
-    visited_nodes = [start_node_id]
-    ttl_history = [initial_ttl]
+    visited_nodes = []
+    ttl_history = []
 
-    current_node = start_node_id
+    current_node_id = start_node_id
     ttl = initial_ttl
 
     while ttl > 0:
-        cached_result = inputGraph.get_cached_result(current_node)
-        if cached_result:
-            return cached_result
-
-        neighbors = inputGraph.get_neighbors(current_node)
-
-        neighbors_with_resource = [neighbor for neighbor in neighbors
-                                   if desiredResource in inputGraph.get_node_resources(neighbor)]
-
-        if not neighbors_with_resource:
-            result = {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
-            inputGraph.update_cache(current_node, result)
-            return result
-
-        next_node = random.choice(neighbors_with_resource)
-
-        current_node = next_node
-        ttl -= 1
-
-        visited_nodes.append(current_node)
+        visited_nodes.append(current_node_id)
         ttl_history.append(ttl)
 
-        if desiredResource in inputGraph.get_node_resources(current_node):
-            result = {"visited": visited_nodes, "found": True, "ttl_history": ttl_history}
-            inputGraph.update_cache(current_node, result)
-            return result
+        if desiredResource in inputGraph.get_node_resources(current_node_id):
+            return {"visited": visited_nodes, "found": True, "ttl_history": ttl_history}
 
-    result = {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
-    inputGraph.update_cache(current_node, result)
-    return result
+        neighbors = [neighbor.node_id for neighbor in inputGraph.data[current_node_id].get_neighbors()]
+
+        if neighbors:
+            current_node_id = random.choice(neighbors)
+        else:
+            break
+        ttl -= 1
+
+    return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
 
 
 def __main():
     json_data = read_json("json_example.json")
     g = Graph(json_data)
-    res = parse_graph(inputGraph=g, graphRestraints=json_data)
-    return start_informed_random_walk_search(inputGraph=g, start_node_id="node_12", desiredResource="starry_skies.mp3",
-                                             initial_ttl=5)
 
+    print("Available Node IDs:", list(g.data.keys()))
+
+    result = start_informed_random_walk_search(g, start_node_id="node_1", desiredResource="dancing_moon.mp3")
+    print(result)
 
 if __name__ == "__main__":
     __main()
+
