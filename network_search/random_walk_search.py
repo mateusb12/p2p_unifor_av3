6920f3c52e46d6312b6bcbe29b5132e51cc98901
isisximenes <isisximenes@hotmail.com>
2023-12-11 18:06:01 -0300

    Update random_walk_search.py

--- network_search/random_walk_search.py
+++ network_search/random_walk_search.py
@@ -7,11 +7,12 @@ import random
 def start_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResource: str, initial_ttl: int = 5) -> dict:
     visited_nodes = [start_node_id]
     ttl_history = [initial_ttl]
+    message_count = 0
 
     current_node = inputGraph.data.get(start_node_id)
 
     if current_node is None:
-        return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
+        return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history, "message_amount": message_count}
 
     ttl = initial_ttl
 
@@ -19,25 +20,25 @@ def start_random_walk_search(inputGraph: Graph, start_node_id: str, desiredResou
         neighbors = current_node.neighbors
 
         if not neighbors:
-            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
+            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history, "message_amount": message_count}
 
         next_node = random.choice(neighbors)
         current_node = inputGraph.data.get(next_node)
 
         if current_node is None:
-            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
+            return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history, "message_amount": message_count}
 
         ttl -= 1
         visited_nodes.append(next_node)
         ttl_history.append(ttl)
+        message_count += 1
 
         print(f"Current Node: {next_node}, TTL: {ttl}, Visited Nodes: {visited_nodes}")
 
         if desiredResource in current_node.resources:
-            return {"visited": visited_nodes, "found": True, "ttl_history": ttl_history}
+            return {"visited": visited_nodes, "found": True, "ttl_history": ttl_history, "message_amount": message_count}
 
-    return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history}
-
+    return {"visited": visited_nodes, "found": False, "ttl_history": ttl_history, "message_amount": message_count}
 
 
 def __main():
