0dffafe05c80c87b4af91e84ef4c1090e5dd1a0a
Mateus <mateus_b09@hotmail.com>
2023-12-11 11:41:31 -0300

    Refactor test handling and add metrics to test data
    The load_tests function in test_results.py has been refactored to include a constant TEST_FILENAME. More importantly, the file now also adds metrics like the number of nodes visited and total messages to the result data, allowing a deeper analysis of the testing output. These changes also include an update to store the augmented data into the original test table file.

--- tests/test_results.py
+++ tests/test_results.py
@@ -7,10 +7,11 @@ from path_references.all_references import get_network_tests_path
 from wrapper.main_pipeline import get_search_result
 
 function_table = {"flooding_search": flooding_search, "informed_flooding_search": informed_flooding_search}
+TEST_FILENAME = "test_table.csv"
 
 
 def load_tests() -> pd.DataFrame:
-    file_path = Path(get_network_tests_path(), "test_table.csv")
+    file_path = Path(get_network_tests_path(), TEST_FILENAME)
     return pd.read_csv(file_path)
 
 
@@ -22,8 +23,11 @@ def loop_through_tests(input_df: pd.DataFrame):
         initial_ttl = row["initial_ttl"]
         chosen_function = function_table[row["function"]]
         result = get_search_result(filename, starting_node, desired_resource, initial_ttl, chosen_function)
-        return
-    return
+        nodes_visited = len(result["visited"])
+        total_messages = result["totalMessages"]
+        row["nodes_visited"] = nodes_visited
+        row["total_messages"] = total_messages
+    input_df.to_csv(Path(get_network_tests_path(), TEST_FILENAME), index=False)
 
 
 def __main():
