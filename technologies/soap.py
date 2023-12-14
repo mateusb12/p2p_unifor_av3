d7c39bde21f442dfd148173f6eea8cf574c8c052
isisximenes <isisximenes@hotmail.com>
2023-12-14 18:00:07 -0300

    Update soap.py

--- technologies/soap.py
+++ technologies/soap.py
@@ -1,95 +1,58 @@
-from spyne import Application, rpc, ServiceBase, Iterable, ComplexModel, Array, Integer, String
+from spyne import Application, rpc, ServiceBase, Iterable, Unicode
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 from wsgiref.simple_server import make_server
+import json
 
+class MusicService(ServiceBase):
+    @rpc(_returns=Iterable(Unicode))
+    def list_all_users(self):
+        with open('../db/users.json', 'r') as file:
+            users_data = json.load(file)
 
-# Defining complex data structures
-class Comment(ComplexModel):
-    _type_info = {
-        'user_id': String,
-        'content': String,
-        'likes': Integer,
-        'replies': Array(String),
-        'created_at': String,
-    }
+        user_names = [user["name"] for user in users_data]
+        return user_names
 
+    @rpc(_returns=Iterable(Unicode))
+    def list_all_songs(self):
+        with open('../db/songs.json', 'r') as file:
+            songs_data = json.load(file)
 
-class Post(ComplexModel):
-    _type_info = {
-        'title': String,
-        'content': String,
-        'comments': Array(Comment),
-        'likes': Integer,
-        'tags': Array(String),
-        'updated_at': String,
-    }
+        songs_data = [song["nome"] for song in songs_data]
+        return songs_data
 
+    @rpc(Unicode, _returns=Iterable(Unicode))
+    def list_user_playlists(self, id_usuario):
+        with open('../db/user_playlists.json', 'r') as file:
+            playlists_data = json.load(file)
 
-class UserActivity(ComplexModel):
-    _type_info = {
-        'likes': Array(String),
-        'comments': Array(String),
-    }
+        user_playlists = [playlist["nome"] for playlist in playlists_data if playlist["id_usuario"] == int(id_usuario)]
+        return user_playlists
 
+    @rpc(Unicode, _returns=Iterable(Unicode))
+    def list_playlist_songs(self, nome):
+        with open('../db/playlists.json', 'r') as file:
+            playlists_data = json.load(file)
 
-class User(ComplexModel):
-    _type_info = {
-        'name': String,
-        'email': String,
-        'friends': Array(String),
-        'posts': Array(String),
-        'activity': UserActivity,
-    }
+        playlist = next((playlist for playlist in playlists_data if playlist["nome"] == nome), None)
 
+        if playlist:
+            songs_in_playlist = playlist.get("songs", [])
+            return songs_in_playlist
+        else:
+            return []
 
-# Adjusted WeatherService to handle the new data structures
-class DataService(ServiceBase):
-    @rpc(String, _returns=User)
-    def getUser(ctx, user_id):
-        # Mock data retrieval logic
-        user = User(
-            name="John Doe",
-            email="john@example.com",
-            friends=["456", "789"],
-            posts=["101", "102"],
-            activity=UserActivity(
-                likes=["201", "202"],
-                comments=["301", "302"]
-            )
-        )
-        return user
-
-    @rpc(String, _returns=Post)
-    def getPost(ctx, post_id):
-        # Mock data retrieval logic
-        post = Post(
-            title="My First Post",
-            content="Hello World!",
-            comments=[Comment(user_id="123", content="Great post!", likes=3, replies=["401"],
-                              created_at="2023-12-02T15:30:00Z")],
-            likes=10,
-            tags=["adventure", "travel"],
-            updated_at="2023-12-01T12:00:00Z"
-        )
-        return post
-
-
-# Creating a SOAP application
-application = Application([DataService],
-                          tns='myapp.soap.data',
+# Create the Spyne application
+application = Application([MusicService], 'music.soap',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
-# Deploy the application using a WSGI server
 if __name__ == '__main__':
-    HOST = '127.0.0.1'
-    PORT = 8000
-    server = make_server(HOST, PORT, WsgiApplication(application))
-    print("SOAP service is running...")
-    print(f"WSDL is at: http://{HOST}:{PORT}/?wsdl")
+    # Wrap the Spyne application with a WsgiApplication
+    wsgi_application = WsgiApplication(application)
 
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        server.shutdown()
+    # Create a simple WSGI server and run it
+    server = make_server('localhost', 8000, wsgi_application)
+    print("Listening on http://localhost:8000...")
+    server.serve_forever()
+
